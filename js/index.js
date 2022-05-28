8d22e16b422a5b90f646bbbc4ee77cb3546f3ab1
Jeff Graham <jeffery.lemar.graham@gmail.com>
2022-05-28 14:45:12 -0400

    add page counter

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let counter = document.getElementById('counter');
+console.log(counter);
+let visitCount = localStorage.getItem('page_view');
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem('page_view', visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem('page_view', 1);
+}
+
+counter.innerHTML = visitCount;
